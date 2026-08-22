67c00259c98467e4c8c8d81adebe6b33901e7594
mateofoulkes-lab <mateofoulkes@gmail.com>
2026-08-22 09:37:01 -0300

    Refresh PWA cache for v3.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='3dlab-mesh-to-cad-v3.1';
+const CACHE='3dlab-mesh-to-cad-v3.2';
 const SHELL=['./','./index.html','./styles.css','./app.js','./mesh-worker.js','./legacy-worker.js','./manifest.webmanifest','./icons/icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
