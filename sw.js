5aaea62c1c0153fde235110e02f80555b2b51d8a
mateofoulkes-lab <mateofoulkes@gmail.com>
2026-08-22 08:17:39 -0300

    Make installed PWA refresh to newest version

--- sw.js
+++ sw.js
@@ -1,19 +1,38 @@
-const CACHE='3dlab-v2';
+const CACHE='3dlab-v3';
 const SHELL=['./','./index.html','./styles.css','./app.js','./mesh-worker.js','./manifest.webmanifest','./icons/icon.svg'];
 
 self.addEventListener('install',event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache=>cache.addAll(SHELL))
+      .then(()=>self.skipWaiting())
+  );
 });
+
 self.addEventListener('activate',event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
 });
+
+self.addEventListener('message',event=>{
+  if(event.data?.type==='SKIP_WAITING') self.skipWaiting();
+});
+
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET') return;
+
   event.respondWith(
-    fetch(event.request).then(response=>{
-      const copy=response.clone();
-      caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});
-      return response;
-    }).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html')))
+    fetch(event.request,{cache:'no-store'})
+      .then(response=>{
+        if(response && response.ok){
+          const copy=response.clone();
+          caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});
+        }
+        return response;
+      })
+      .catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html')))
   );
 });
