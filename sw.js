5fa0015e8518d056029996f1ac9a9f1ec54ad036
mateofoulkes-lab <mateofoulkes@gmail.com>
2026-08-22 10:31:13 -0300

    Update PWA cache for assisted v5

--- sw.js
+++ sw.js
@@ -1,12 +1,6 @@
-const CACHE='3dlab-mesh-to-cad-v4.0';
-const SHELL=['./','./index.html','./styles.css','./app.js','./mesh-worker.js','./legacy-worker.js','./rebuild-worker.js','./manifest.webmanifest','./icons/icon.svg'];
+const CACHE='3dlab-assisted-v5.0';
+const SHELL=['./','./index.html','./styles.css','./app.js','./assisted-worker.js','./manifest.webmanifest','./icons/icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('message',event=>{if(event.data?.type==='SKIP_WAITING')self.skipWaiting()});
-self.addEventListener('fetch',event=>{
- if(event.request.method!=='GET')return;
- const url=new URL(event.request.url);
- if(url.origin===location.origin){
-  event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{if(response.ok){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{})}return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))));
- }
-});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;const url=new URL(event.request.url);if(url.origin===location.origin){event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{if(response.ok){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{})}return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))))}});
